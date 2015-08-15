7c9ee4444856bdc0b25b9153f0206a94d6c4d17a
shayan <dochead@gmail.com>
2015-08-15 12:46:30 +0200

    Added additional team attributes & match test

--- leaguetable/team.py
+++ leaguetable/team.py
@@ -2,5 +2,31 @@ class Team(object):
     def __init__(self, team_name):
         self.team_name = team_name
 
+        self.matches = []
+
+        self.won = 0
+        self.drew = 0
+        self.lost = 0
+        self.goals_for = 0
+        self.goals_against = 0
+
     def __repr__(self):
-        return '{}'.format(self.team_name)
+        return '{} - Pl: {}, W: {}, L: {}, D:{}, GF: {}, GA: {}, GD: {}'.format(
+            self.team_name,
+            len(self.matches),
+            self.played,
+            self.won,
+            self.drew,
+            self.lost,
+            self.goals_for,
+            self.goals_against,
+            self.goal_difference
+        )
+
+    @property
+    def played(self):
+        return self.won + self.drew + self.lost
+
+    @property
+    def goal_difference(self):
+        return self.goals_for - self.goals_against
