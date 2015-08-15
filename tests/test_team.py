53df95a8a96cfc01eea4b395cb0eef91c6af0eec
shayan <dochead@gmail.com>
2015-08-15 15:03:15 +0200

    Oer, forgot to add these files somehow

---
+++ tests/test_team.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-.
+
+import unittest
+
+from leaguetable.team import Team
+
+
+class TestTeam(unittest.TestCase):
+    def test_team_define(self):
+        t = Team(u'Liddlypool')
+        self.assertEqual(unicode(t), u'Liddlypool')
+
+    def test_match_international(self):
+        away = Team(u'Александра')
+        self.assertEqual(away.team_name, u'Александра')
+
+
+if __name__ == '__main__':
+    unittest.main()
